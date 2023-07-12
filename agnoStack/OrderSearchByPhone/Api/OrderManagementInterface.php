8d70e2b6f5bd87d3d567127bf069f1b98b05a859
Rob Bowers <rob.bowers@gmail.com>
2023-07-12 09:48:48 -0400

    chore: complete working version

--- agnoStack/OrderSearchByPhone/Api/OrderManagementInterface.php
+++ agnoStack/OrderSearchByPhone/Api/OrderManagementInterface.php
@@ -3,11 +3,11 @@ namespace agnoStack\OrderSearchByPhone\Api;
 
 interface OrderManagementInterface
 {
-    /**
-     * GET order by phone
-     *
-     * @param string $phone
-     * @return \Magento\Sales\Api\Data\OrderInterface[]
-     */
-    public function getOrderByPhone($phone);
+  /**
+   * GET order by phone
+   *
+   * @param string $phone
+   * @return \Magento\Sales\Api\Data\OrderInterface[]
+   */
+  public function getOrderByPhone($phone);
 }
--- agnoStack/OrderSearchByPhone/Model/OrderManagement.php
+++ agnoStack/OrderSearchByPhone/Model/OrderManagement.php
@@ -2,25 +2,36 @@
 namespace agnoStack\OrderSearchByPhone\Model;
 
 use agnoStack\OrderSearchByPhone\Api\OrderManagementInterface;
-use Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
+use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
+use Magento\Sales\Model\ResourceModel\Order\Address\CollectionFactory as AddressCollectionFactory;
 
 class OrderManagement implements OrderManagementInterface
 {
-    protected $orderCollectionFactory;
+  protected $orderCollectionFactory;
+  protected $addressCollectionFactory;
 
-    public function __construct(CollectionFactory $orderCollectionFactory)
-    {
-        $this->orderCollectionFactory = $orderCollectionFactory;
-    }
+  public function __construct(
+    OrderCollectionFactory $orderCollectionFactory,
+    AddressCollectionFactory $addressCollectionFactory
+  )
+  {
+    $this->orderCollectionFactory = $orderCollectionFactory;
+    $this->addressCollectionFactory = $addressCollectionFactory;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getOrderByPhone($phone)
-    {
-        $collection = $this->orderCollectionFactory->create();
-        $collection->addFieldToFilter('shipping_address.telephone', ['eq' => $phone]);
+  /**
+   * {@inheritdoc}
+   */
+  public function getOrderByPhone($phone)
+  {
+    $addressCollection = $this->addressCollectionFactory->create();
+    $addressCollection->addFieldToFilter('telephone', ['eq' => $phone]);
 
-        return $collection->getItems();
-    }
+    $orderIds = $addressCollection->getColumnValues('parent_id');
+
+    $collection = $this->orderCollectionFactory->create();
+    $collection->addFieldToFilter('entity_id', ['in' => $orderIds]);
+
+    return $collection->getItems();
+  }
 }
